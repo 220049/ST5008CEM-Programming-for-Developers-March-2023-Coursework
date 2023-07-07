75cb845d24159e999ee9e216819d876fb3acf8f7
220049 <220049@softwarica.edu.np>
2023-07-07 21:40:55 +0545

    2a answer

--- Question 2a.py
+++ Question 2a.py
@@ -16,3 +16,23 @@ The subsequence has a length of 5, so we return 5.
 Note that the subsequence [1,3,4,5,8,15] does not meet the requirements because 15 - 8 = 7 is larger 
 than 3.
 '''
+
+def longestSubsequence(nums, k):
+    n = len(nums)
+    dp = [1] * n  # Initialize a dynamic programming array with all elements set to 1
+    maxLen = 0  # Initialize the maximum length variable as 0
+
+    for i in range(n):  # Iterate over the elements of nums
+        for j in range(i):  # Iterate over the elements before the current element
+            if nums[i] - nums[j] <= k:  # Check if the difference between elements is at most k
+                dp[i] = max(dp[i], dp[j] + 1)  # Update the length of the subsequence if condition is met
+        maxLen = max(maxLen, dp[i])  # Update the maximum length variable
+
+    return maxLen  # Return the maximum length of the subsequence
+
+
+# Example usage:
+nums = [8, 5, 4, 2, 1, 4, 3, 4, 3, 1, 15]
+k = 3
+result = longestSubsequence(nums, k)
+print(result)  # Output: 5
