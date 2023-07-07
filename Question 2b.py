d429be72e26207690d50ad7bbdcd76311465d77a
220049 <220049@softwarica.edu.np>
2023-07-07 21:44:38 +0545

    2b answer

--- Question 2b.py
+++ Question 2b.py
@@ -20,3 +20,25 @@ p.get(); // return 0, any number from [0,1,4,6] should be ok. Note that for ever
 // 0, 1, 4, and 6 must be equally likely to be returned (i.e., with probability 1/4).
 p.get(); // return 4
 '''
+import random
+
+class Program:
+    def __init__(self, k, blacklisted_ports):
+        self.k = k
+        self.blacklisted_ports = set(blacklisted_ports)  # Convert blacklisted_ports to a set for efficient lookup
+        self.whitelisted_ports = []  # Initialize the list to store whitelisted ports
+
+    def get(self):
+        if len(self.whitelisted_ports) == self.k - len(self.blacklisted_ports):
+            return None  # All whitelisted ports have been generated
+        
+        while True:
+            port = random.randint(0, self.k - 1)  # Generate a random port between 0 and k-1
+            if port not in self.blacklisted_ports:  # Check if the port is not blacklisted
+                self.whitelisted_ports.append(port)  # Add the port to the list of whitelisted ports
+                return port
+
+# Example usage:
+p = Program(7, [2, 3, 5])  # Create an instance of the Program class with k=7 and blacklisted_ports=[2, 3, 5]
+print(p.get())  # Output: A whitelisted random port (e.g., 0)
+print(p.get())  # Output: Another whitelisted random port (e.g., 4)
